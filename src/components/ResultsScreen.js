725f471230f662467ef6cf1e456920663f6617d1
smorriswisc <smorris24@wisc.edu>
2023-05-07 16:44:42 -0500

    Add link to presentation

--- src/components/ResultsScreen.js
+++ src/components/ResultsScreen.js
@@ -16,8 +16,8 @@ function ResultsScreen() {
 			<p>My model achieved an upper accuracy of 72.0%, while the online model reached 72.1%.</p>
 			<p>As you play around with the embedded model, you will notice that the accuracy does seem to be around 72%.</p>
 			
-			<p>My code repository can be found here <a href = "https://github.com/smorriswisc/ComputerVisionProject">https://github.com/smorriswisc/ComputerVisionProject</a></p>
-		
+			<p>My code repository can be found here: <a href = "https://github.com/smorriswisc/ComputerVisionProject">https://github.com/smorriswisc/ComputerVisionProject</a></p>
+			<p>My project presentaiton can be found here: <a href="https://uwprod-my.sharepoint.com/:p:/g/personal/smorris24_wisc_edu/EYd9EmXOTshArs2LiWH4EzgB4jKy0F8DtyJdX3FOU5eUMw?e=3WCweI">CompVision Presentation</a></p>
 		</Container>
 
     );
